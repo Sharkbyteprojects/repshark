32e754c080442e78db043f4eaa146b35c9aa76a5
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2019-12-18 18:11:05 +0100

    Add Package

---
+++ rep.sh.js
@@ -0,0 +1,8 @@
+/**
+*
+* REP.SH
+* WRITTEN BY SHARKBYTEPROJECTS
+*/
+export default function repsh(strings, sfor, repwit) {
+  return strings.split(sfor).join(repwit)
+}
---
+++ test.js
@@ -0,0 +1,2 @@
+import repsh from "./rep.sh.js";
+console.log("Input: repsh(\"Test Javascript\", 'Test', 'Sharktest')\n Output:\t"+repsh("Test Javascript", "Test", "Sharktest"));
