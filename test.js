ba29371ce2fd6ccf1ab6ea2084100b12fb50c528
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2019-12-23 12:15:05 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,2 @@
-const repsh = require("./rep.sh.js");
+const repsh = require("./index.js");
 console.log("Input: repsh(\"Test Javascript\", 'Test', 'Sharktest')\n Output:\t"+repsh("Test Javascript", "Test", "Sharktest"));
