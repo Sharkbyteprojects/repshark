c86b3177746316d79498270153ea5bc4a5111afb
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2019-12-23 12:04:27 +0100

    Update rep.sh.js

--- rep.sh.js
+++ rep.sh.js
@@ -3,6 +3,6 @@
 * REP.SH
 * WRITTEN BY SHARKBYTEPROJECTS
 */
-export default function repsh(strings, sfor, repwit) {
+module.export=(strings, sfor, repwit)=> {
   return strings.split(sfor).join(repwit)
 }
